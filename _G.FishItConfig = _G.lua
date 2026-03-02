315fe566d17fd34626ad02fcd6aa7bb6711c9d05
gembil-moo <64587345+gembil-moo@users.noreply.github.com>
2026-03-02 09:11:35 +0700

    Update _G.FishItConfig = _G.lua

--- _G.FishItConfig = _G.lua
+++ _G.FishItConfig = _G.lua
@@ -21,7 +21,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Auto Trade"] = {
         ["Enabled"] = true,
-        ["Whitelist Username"] = {"wokrkerdivine01","wokrkerdivine02","wokrkerdivine03","wokrkerdivine04","wokrkerdivine05","pekerjadvn01","pekerjadvn02","pekerjadvn03","pekerjadvn04","pekerjadvn05"},
+        ["Whitelist Username"] = {"workerdivine01","workerdivine02","workerdivine03","workerdivine04","workerdivine05","pekerjadvn01","pekerjadvn02","pekerjadvn03","pekerjadvn04","pekerjadvn05"},
         ["Category Fish"] = {
             "Secret", "Mythic", 
         },
